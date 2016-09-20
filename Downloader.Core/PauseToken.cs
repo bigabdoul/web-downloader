7beaaf5c7ff1d9a43a34587f25c530cf2fdf993b
Abdul Kaba <kababdoul@hotmail.com>
2016-09-20 13:14:24 +0000

    Xml-documented the PauseToken structure and PauseTokenSource class

--- Downloader.Core/PauseToken.cs
+++ Downloader.Core/PauseToken.cs
@@ -2,16 +2,30 @@
 
 namespace Downloader.Core
 {
+    /// <summary>
+    /// Represents a token for pausing code execution.
+    /// </summary>
     public struct PauseToken
     {
+        /// <summary>
+        /// Represents the default pause token.
+        /// </summary>
         public static readonly PauseToken None = new PauseToken(new PauseTokenSource());
 
         private readonly PauseTokenSource m_source;
 
         internal PauseToken(PauseTokenSource source) { m_source = source; }
 
+        /// <summary>
+        /// Gets a value that indicates whether current token source is paused.
+        /// </summary>
         public bool IsPaused { get { return m_source != null && m_source.IsPaused; } }
 
+        /// <summary>
+        /// Blocks the calling thread when the current token source is actually paused.
+        /// Otherwise, returns control immediately to the calling thread.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> instance.</returns>
         public Task WaitWhilePausedAsync()
         {
             return IsPaused ?
